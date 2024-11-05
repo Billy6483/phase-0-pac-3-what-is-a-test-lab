7c5e9139b514f2b6ab5bdc255db1ad8b80deb6a4
Billy6483 <koechkbilly@gmail.com>
2024-11-05 00:09:01 -0500

    running tests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan"; // Change "Joe" to "Susan"
+const height = 30; // Change 74 to a value less than 40 and greater than 0
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
